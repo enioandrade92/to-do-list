4abcebef6e54f7acf4ac5c7248c3693f1e468d37
Enio Andrade <enio.goulart@gmail.com>
2024-10-04 17:08:22 -0300

    fix: added  Health Check  tag swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Health Check')
 @Controller()
 export class AppController {
-  @Get('/health_check')
-  async healthCheck() {
-    return { alive: true };
-  }
+    @Get('/health_check')
+    async healthCheck() {
+        return { alive: true };
+    }
 }
