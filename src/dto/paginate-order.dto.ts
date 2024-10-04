6dc439764c7a25b4dbd1e1eadd1631e663569376
Enio Andrade <enio.goulart@gmail.com>
2024-10-04 17:09:24 -0300

    feat: PaginateAndOrderDto

---
+++ src/dto/paginate-order.dto.ts
@@ -0,0 +1,20 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsIn, IsNumberString, IsOptional, IsString } from 'class-validator';
+
+export class PaginateAndOrderDto {
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    @IsIn(['ASC', 'DESC', 'asc', 'desc'])
+    order?: 'ASC' | 'DESC' | 'asc' | 'desc';
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsNumberString()
+    page? = 1;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsNumberString()
+    limit? = 10;
+}
