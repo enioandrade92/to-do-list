2b46b189d494b6187fe963f696308c1eafce8343
Enio Andrade <enio.goulart@gmail.com>
2024-10-04 17:13:10 -0300

    feat: TaskStatus enum

---
+++ src/modules/task/@types/task-status.enum.ts
@@ -0,0 +1,8 @@
+export enum TaskStatus {
+    TO_DO = 'To Do',
+    IN_PROGRESS = 'In Progress',
+    COMPLETED = 'Completed',
+    ON_HOLD = 'On Hold',
+    CANCELED = 'Canceled',
+    ARCHIVED = 'Archived',
+}
