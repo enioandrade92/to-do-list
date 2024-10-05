4822386498f56b4021f02796576eb331817c9758
Enio Andrade <enio.goulart@gmail.com>
2024-10-05 12:04:19 -0300

    feat(task service): createSubtask method

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -7,15 +7,36 @@ import { Task } from './entities/task.entity';
 import { FindManyOptions, IsNull, Repository } from 'typeorm';
 import { paginate } from 'nestjs-typeorm-paginate';
 import { GetTasksFiltersDto } from './dto/get-tasks-filters.dto';
-import { GetSubTasksFiltersDto } from './dto/get-subtasks-filters.dto';
+import { CreateSubTaskDto } from './dto/create-subtask.dto';
 
 @Injectable()
 export class TaskService {
-    constructor(@InjectRepository(Task) private repositoryTask: Repository<Task>) {}
+    private readonly relationFields = { subtasks: true, createdBy: true };
+    constructor(
+        @InjectRepository(Task) private taskRepository: Repository<Task>,
+    ) {}
 
     async createTask(createTaskDto: CreateTaskDto, user: UserJwtPayload) {
-        return await this.repositoryTask.save({
+        return await this.taskRepository.save({
             ...createTaskDto,
+            createdBy: { id: user.id },
+        });
+    }
+
+    async createSubtask(
+        createSubTaskDto: CreateSubTaskDto,
+        user: UserJwtPayload,
+    ) {
+        const parent = await this.taskRepository.findOneBy({
+            id: createSubTaskDto.parent.id,
+        });
+        if (!parent)
+            throw new NotFoundException(
+                `task parent id: ${createSubTaskDto.parent.id}, not found`,
+            );
+
+        return await this.taskRepository.save({
+            ...createSubTaskDto,
             createdBy: { id: user.id },
         });
     }
@@ -23,38 +44,53 @@ export class TaskService {
     async getTasks(filters: GetTasksFiltersDto) {
         const { page, limit } = filters;
         const findOptions: FindManyOptions<Task> = {
+            order: { createdAt: filters?.order ?? 'DESC' },
+            relations: this.relationFields,
             where: {
-                parent: IsNull(),
+                parent: filters?.parent?.id
+                    ? { id: filters.parent.id }
+                    : IsNull(),
             },
-            order: { createdAt: filters.order ?? 'DESC' },
         };
 
-        return paginate(this.repositoryTask, { page, limit }, findOptions);
-    }
+        if (filters?.status) {
+            findOptions.where = {
+                ...findOptions.where,
+                status: filters.status,
+            };
+        }
 
-    async getSubTasks(filters: GetSubTasksFiltersDto) {
-        const { page, limit } = filters;
-        const findOptions: FindManyOptions<Task> = {
-            where: {
-                parent: { id: filters.parent.id },
-            },
-            order: { createdAt: filters.order ?? 'DESC' },
-        };
-
-        return paginate(this.repositoryTask, { page, limit }, findOptions);
+        return await paginate(
+            this.taskRepository,
+            { page, limit },
+            findOptions,
+        );
     }
 
     async getTaskById(id: string) {
-        const task = await this.repositoryTask.findOneBy({ id });
+        const task = await this.taskRepository.findOne({
+            where: { id },
+            relations: this.relationFields,
+        });
         if (!task) throw new NotFoundException(`task id: ${id}, not found`);
         return task;
     }
 
     async updateTask(id: string, updateTaskDto: UpdateTaskDto) {
-        return `This action updates a #${id} task`;
+        // In some cases it is necessary to verify who created it in order to edit it
+        const task = await this.taskRepository.findOneBy({ id });
+        if (!task) throw new NotFoundException(`task id: ${id}, not found`);
+
+        return await this.taskRepository.save({
+            ...task,
+            ...updateTaskDto,
+        });
     }
 
     async removeTask(id: string) {
-        return `This action removes a #${id} task`;
+        // In some cases it is necessary to verify relations and who created it in order to remove it
+        const task = await this.taskRepository.findOneBy({ id });
+        if (!task) throw new NotFoundException(`task id: ${id}, not found`);
+        return await this.taskRepository.softRemove(task);
     }
 }
